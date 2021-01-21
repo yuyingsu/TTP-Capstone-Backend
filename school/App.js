227113658ccdd699c38295d12d6c240739b9f7e8
yuyingsu <yuyingsujob@gmail.com>
2021-01-20 21:42:17 -0500

    revised version

--- school/App.js
+++ school/App.js
@@ -2,14 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const models = require('./models/index');
-
+const indexRouter = require('./routes/index');
+const path = require('path');
 const app = express();
 app.use(cors());
-app.use(require('./routes/index'));
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use('/', indexRouter);
+app.use(express.static(path.join(__dirname, 'public')));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
