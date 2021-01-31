95b6efb737a824a43ffe7b6ea48782a5757702b2
Scarcrux <reg@scarcrux.com>
2021-01-31 12:12:30 -0500

    1st Commit

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT || 5000,
+  URI: process.env.MONGODB_URI ||'mongodb+srv://Yuying:gamerbackend@gamer.4dbbj.mongodb.net/test',
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/gamerdotcom',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
